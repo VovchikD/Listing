bddcdc8c60cb2d848329016b8a84501f8fdc860c
Volodia <hryhorskyiv@gmail.com>
2024-06-16 23:53:14 +0300

    fix image attachment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_160227) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_16_202016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,7 +66,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_160227) do
     t.float "engine_capacity"
     t.string "phone_number"
     t.string "name"
-    t.string "image"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
